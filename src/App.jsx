804a1a0eff9a7205a83bfb55680caf880e56bcf4
Julius Adebowale Gabriel <juadebgabriel@gmail.com>
2026-06-13 14:22:14 +0100

    feat; fix

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const App = () => {
+  return (
+    <div className="bg-black text-white flex items-center justify-center min-h-full">
+      <div className="h-screen w-full text-center flex items-center justify-center">
+        App
+      </div>
+    </div>
+  );
+};
+
+export default App;
